23d6ef3c18f272eea022b11245355baac8dbed67
Luxarions <bay.nisdhilla7@gmail.com>
2026-07-28 16:27:08 +0700

    feat: export LXRN namespace and math modules
    Consolidate the library's core mathematical and geometric modules into a unified LXRN namespace for easier access and improved library organization.

--- src/index.js
+++ src/index.js
@@ -51,3 +51,88 @@ export * from './math/CatmullRom.js';
 export * from './math/CurvePath.js';
 export * from './math/Surface.js';
 export * from './math/Interpolant.js';
+
+// LXRN Namespace
+import * as AccordionModule from './components/Accordion.js';
+import * as MathUtilsModule from './math/MathUtils.js';
+import * as Vec2Module from './math/Vec2.js';
+import * as Vec3Module from './math/Vec3.js';
+import * as Vec4Module from './math/Vec4.js';
+import * as Mat2Module from './math/Mat2.js';
+import * as Mat3Module from './math/Mat3.js';
+import * as Mat4Module from './math/Mat4.js';
+import * as MatrixModule from './math/Matrix.js';
+import * as MatrixStackModule from './math/MatrixStack.js';
+import * as QuatModule from './math/Quat.js';
+import * as EulerModule from './math/Euler.js';
+import * as DualQuatModule from './math/DualQuat.js';
+import * as ComplexModule from './math/Complex.js';
+import * as Bound2Module from './math/Bound2.js';
+import * as Bound3Module from './math/Bound3.js';
+import * as OBBModule from './math/OBB.js';
+import * as SphereModule from './math/Sphere.js';
+import * as PlaneModule from './math/Plane.js';
+import * as RayModule from './math/Ray.js';
+import * as FrustumModule from './math/Frustum.js';
+import * as TriangleModule from './math/Triangle.js';
+import * as Triangle2Module from './math/Triangle2.js';
+import * as Line2Module from './math/Line2.js';
+import * as Line3Module from './math/Line3.js';
+import * as IntersectionModule from './math/Intersection.js';
+import * as CapsuleModule from './math/Capsule.js';
+import * as CylinderModule from './math/Cylinder.js';
+import * as ConeModule from './math/Cone.js';
+import * as TorusModule from './math/Torus.js';
+import * as BezierModule from './math/Bezier.js';
+import * as SplineModule from './math/Spline.js';
+import * as BSplineModule from './math/BSpline.js';
+import * as NURBSModule from './math/NURBS.js';
+import * as HermiteModule from './math/Hermite.js';
+import * as CatmullRomModule from './math/CatmullRom.js';
+import * as CurvePathModule from './math/CurvePath.js';
+import * as SurfaceModule from './math/Surface.js';
+import * as InterpolantModule from './math/Interpolant.js';
+
+const LXRN = {
+  Accordion: AccordionModule,
+  MathUtils: MathUtilsModule,
+  Vec2: Vec2Module,
+  Vec3: Vec3Module,
+  Vec4: Vec4Module,
+  Mat2: Mat2Module,
+  Mat3: Mat3Module,
+  Mat4: Mat4Module,
+  Matrix: MatrixModule,
+  MatrixStack: MatrixStackModule,
+  Quat: QuatModule,
+  Euler: EulerModule,
+  DualQuat: DualQuatModule,
+  Complex: ComplexModule,
+  Bound2: Bound2Module,
+  Bound3: Bound3Module,
+  OBB: OBBModule,
+  Sphere: SphereModule,
+  Plane: PlaneModule,
+  Ray: RayModule,
+  Frustum: FrustumModule,
+  Triangle: TriangleModule,
+  Triangle2: Triangle2Module,
+  Line2: Line2Module,
+  Line3: Line3Module,
+  Intersection: IntersectionModule,
+  Capsule: CapsuleModule,
+  Cylinder: CylinderModule,
+  Cone: ConeModule,
+  Torus: TorusModule,
+  Bezier: BezierModule,
+  Spline: SplineModule,
+  BSpline: BSplineModule,
+  NURBS: NURBSModule,
+  Hermite: HermiteModule,
+  CatmullRom: CatmullRomModule,
+  CurvePath: CurvePathModule,
+  Surface: SurfaceModule,
+  Interpolant: InterpolantModule
+};
+
+export default LXRN;
